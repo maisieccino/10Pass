8065e724cffabbfdc419ea11ed69cf57379209d6
logicnotlogical <logicnotlogical@outlook.com>
2015-08-17 19:11:23 +0100

    Added menu options.

--- 10Pass/Models/NavMenu.cs
+++ 10Pass/Models/NavMenu.cs
@@ -19,7 +19,9 @@ namespace _10Pass.Models
             MenuItems = new List<NavItem>()
             {
                 new NavItem("\uE10F","Home",typeof(PageHome),FG),
-                new NavItem("\uE109","Add Pass",typeof(PageAddPass),FG)
+                new NavItem("\uE109","Create A Pass",typeof(PageAddPass),FG),
+                new NavItem("\uEA8F","Notifications",typeof(BlankPage),FG),
+                new NavItem("\uE713","Settings",typeof(BlankPage),FG)
             };
             {
 
